4993685b3f979312b638c915c99920435608eaba
mericvatan <mericvatan12@hotmail.com>
2023-07-03 01:16:52 +0300

    MainMenu sahnesi oluşturuldu ve Settings menüsü oluşturuldu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,14 @@
+using UnityEngine.SceneManagement;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.Audio;
+
+public class MainMenu : MonoBehaviour
+{
+
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
